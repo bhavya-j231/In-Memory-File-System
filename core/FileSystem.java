0c89b09062f215a37287ba663629f660c63f3694
Akash3206 <224g1a3206@srit.ac.in>
2026-05-28 12:59:05 +0530

    new Commit

--- core/FileSystem.java
+++ core/FileSystem.java
@@ -1,6 +1,74 @@
 package core;
 
+import java.util.List;
+import entity.*;
+import permissions.Permission;
+
 public class FileSystem {
 
-    
+    private static FileSystem instance;
+    private Directory root;
+
+    private FileSystem() {
+        this.root = new Directory("root", new Permission(true, true, true, true, true));
+    }
+
+    public static FileSystem getInstance() {
+        if(instance == null)
+            instance = new FileSystem();
+        return instance;
+    }
+
+    public void mkdir(String path, Permission permission) {
+
+        int lastIndex = path.lastIndexOf("/");
+        if(lastIndex <= 0)
+            throw new RuntimeException("Invalid path");
+
+        String directoryName = path.substring(lastIndex + 1);
+
+        Directory parent = PathResolver.resolveParent(root, path);
+
+        Directory newDirectory = FileSystemFactory.createDirectory(directoryName, permission);
+        
+        parent.addChild(newDirectory);
+    }
+
+    public void touch(String path, Permission permission, String content, String fileType) {
+        int lastIndex = path.lastIndexOf("/");
+        if(lastIndex <= 0)
+            throw new RuntimeException("Invalid path");
+
+        String fileName = path.substring(lastIndex + 1);
+
+        Directory parent = PathResolver.resolveParent(root, path);
+
+        File newFile = FileSystemFactory.createFile(fileName, permission, content, fileType);
+
+        parent.addChild(newFile);    
+    }
+
+    public void rm(String path) {
+
+    }
+
+    public String cat(String path) {
+        return "";
+    }
+
+    public void write(String path, String content) {
+
+    }
+
+    public List<FileSystemEntity> ls(String path) {
+        return null;
+    }
+
+    public void undo(String path) {
+
+    }
+
+    public void redo(String path) {
+        
+    }
 }
--- core/PathResolver.java
+++ core/PathResolver.java
@@ -1,12 +1,64 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.List;
 import entity.*;
+
 public class PathResolver {
 
     public static FileSystemEntity resolve(Directory root, String path) {
-        String[] parts = path.split("/");
-
         
-        return null;
+        List<String> parts = getPathParts(path);
+        int n = parts.size();
+
+        if(n == 0)
+            throw new RuntimeException("Invalid Path");
+
+        if(!parts.get(0).equals(root.getName()))
+            throw new RuntimeException("Invalid root path");
+
+        FileSystemEntity current = root;
+        for(int i = 1; i < n; i++) {
+            if(!(current instanceof Directory))
+                throw new RuntimeException("Invalid path"); 
+
+            Directory currentDirectory = (Directory) current; 
+
+            FileSystemEntity child = currentDirectory.getChild(parts.get(i));
+
+            if(child == null)
+                throw new RuntimeException("Path does not exist");
+
+            current = child;
+        }
+        return current;
+    }
+
+    public static Directory resolveParent(Directory root, String path) {
+        int ind = path.lastIndexOf("/");
+
+        if(ind <= 0) 
+            throw new RuntimeException("Root has no parent");
+
+        path = path.substring(0, ind);
+
+        FileSystemEntity parent = resolve(root, path);
+
+        if(!(parent instanceof Directory)) 
+            throw new RuntimeException("Parent is not a directory");
+
+        return (Directory) parent;
+    }
+    
+    public static List<String> getPathParts(String path) {
+        String[] strs = path.split("/");
+
+        List<String> parts = new ArrayList<>();
+        for(String s : strs) {
+            if(!s.isEmpty())
+                parts.add(s);
+        }
+
+        return parts;
     }
 }
